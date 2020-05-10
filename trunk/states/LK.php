6bbda62e1ba0d386964d09d0e776ed8ece80b293
Yazeer <mohammedyazir1@gmail.com>
2020-05-10 22:03:17 +0530

    Update and rename templates/states/XX.php to trunk/states/LK.php

---
+++ trunk/states/LK.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * $country states/provinces/regions
+ * - $count states/provinces/regions
+ * 
+ * Source: 
+ * - [https://www.lankapropertyweb.com/property-news/colombo-1-to-15-city-names-are-you-familiar-with-these/]
+ *
+ * @author  Yazeer  | 
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+
+$states['LK'] = array(
+ 
+  'COL' => 'COLOMBO 1-15',
+  'ROC' => 'OTHER CITIES',
+  
+);
