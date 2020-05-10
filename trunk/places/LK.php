8ad8c36eba983c9035395a064fb3767605923a32
Yazeer <mohammedyazir1@gmail.com>
2020-05-10 21:59:30 +0530

    Update and rename templates/places/XX.php to trunk/places/LK.php

---
+++ trunk/places/LK.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * $country places/cities/municipalities/districts
+ * - $count places/cities/municipalities/districts
+ * 
+ * Source: 
+ * - [https://en.wikipedia.org/wiki/Template:Suburbs_of_Colombo]
+ * 
+ * @author  Yazeer 
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $places;
+
+$places['LK'] = array(
+
+  'COL' => array(
+   
+    'Colombo Fort',
+    'Slave Island',
+    'Kolpity',
+    'Bambalapitiya',
+    'Narahenpita, Havelock Town , Kirulapona North',
+    'Wellawatta, Pamankada, Kirulapona South',
+    'Cinnamon Garden',
+    'Borella',
+    'Dematagoda',
+    'Maradana, Panchikawatte',
+    'Pettah',
+    'Hulsfdorf',
+     'Grandpass',
+    'Bloemendhal',
+     'Mattakkuliya, Modara, Mutwal, Madampitiya',
+    
+    
+  ),
+  'ROC' => array(
+    'Dehiwala',
+    'Kalubowila',
+    'Kohuwala',
+    'Mount-Lavinia',
+    'Ratmalana',
+  ),
+ 
+);
