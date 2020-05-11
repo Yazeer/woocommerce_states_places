5526cc28d340255958909ea3bd24a147e8725c27
Yazeer <mohammedyazir1@gmail.com>
2020-05-11 14:50:29 +0530

    Update and rename templates/states/XX.php to trunk/states/LK.php

---
+++ trunk/states/LK.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * $country states/provinces/regions
+ * - $count states/provinces/regions
+ * 
+ * Source: 
+ * - [http://www.lankalinksystems.com/directory/newsinfo/slpostcode1.htm]
+ *
+ * @author  Yazeer  | 
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+
+
+$states['LK'] = array(
+ 
+  'CO' => 'Colombo',
+  'GQ' => 'Gampaha',
+  'KT' => 'Kalutara',
+  'KY' => 'Kandy',
+  'MT' => 'Matale',
+  'NW' => 'Nuwaraeliya',
+  'BC' => 'Batticalo',
+  'TC' => 'Trincomalee',
+  'APR' => 'Ampara',
+  'JA' => 'Jaffna',
+  'MB' => 'Mannar',
+  'MP' => 'Mullativu',
+  'AD' => 'Anuradhapura',
+  'PR' => 'Polonnaruwa',
+  'KG' => 'Kurunegala',
+ 
+  
+  
+  
+  
+  
+  
+);
